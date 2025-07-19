b1deade0bb12b06e4dd0442307f9bd5568611607
Marcos-Ramoss <mar.stark.99@gmail.com>
2025-07-19 19:57:13 -0400

    Atualiza configurações do MySQL no docker-compose, altera nome do serviço e remove arquivos de relatórios antigos. Adiciona script de inserção de usuários na base de dados e ajusta o arquivo application.yml para refletir as novas credenciais e nome do banco de dados.

---
+++ backend/src/main/resources/db/migration/V4__insert_usuarios.sql
@@ -0,0 +1,7 @@
+INSERT INTO gestao.usuario
+(id, username, password, `role`, nome, cpf)
+VALUES(1, 'marcos', '$2a$10$VZwPoI9f6g/fnGpD0Zhk4uKV0NKFbpJnlDPP8CbGxy1rU5YtQECbO', 'USER', 'Marcos Oliveira', '11120030011');
+
+INSERT INTO gestao.usuario
+(id, username, password, `role`, nome, cpf)
+VALUES(2, 'Claudemir', '$2a$10$O9kuRflXAaAFxzCTOA2eAeZkOoft5ueQ/wJZ6fGzj.snYpg5k9CEq', 'USER', 'Claudemir Ramos', '11112222223'); 
